2a63e64fb60ac4302efb03158fb0e9a2536044cb
ridler <forrest.ridler@colorado.edu>
2015-02-23 22:47:33 -0700

    added oembed endpoint

---
+++ apps/get_oembed.rb
@@ -0,0 +1,64 @@
+require_relative '../requests/StatusesOembed'
+
+require 'trollop'
+
+USAGE = %Q{
+get_oembed: returns embeddable HTML for a given tweet.
+
+Usage:
+
+  ruby get_oembed.rb <options> <id>
+
+  <id>: A Tweet ID.
+
+The following options are required:
+
+  --params: An OAuth Properties File
+}
+
+def parse_command_line
+
+  options = {type: :string, required: true}
+
+  opts = Trollop::options do
+    version "get_tweets 0.1 (c) 2015 Kenneth M. Anderson"
+    banner USAGE
+    opt :props, "OAuth Properties File", options
+  end
+
+  unless File.exist?(opts[:props])
+    Trollop::die :props, "must point to a valid oauth properties file"
+  end
+
+  opts[:id] = ARGV[0]
+
+  if opts[:id] == nil
+    Trollop::die "You must supply a tweet ID"
+  end
+
+  opts
+end
+
+if __FILE__ == $0
+
+  STDOUT.sync = true
+
+  input  = parse_command_line
+  params = { id: input[:id] }
+  data   = { props: input[:props] }
+
+  args   = { params: params, data: data }
+
+  tweet = StatusesOembed.new(args)
+
+  puts "Getting HTML for tweet id '#{input[:id]}'"
+
+  File.open('tweet.json', 'w') do |f|
+    tweet.collect do |tweet|
+      f.puts "#{tweet.to_json}\n"
+    end
+  end
+
+  puts "DONE."
+
+end
