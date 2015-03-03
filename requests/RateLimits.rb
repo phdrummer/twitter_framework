336b0b16c25a3c7d489fe6efed30c54ab8646530
Ken Anderson <ken.anderson@colorado.edu>
2015-03-03 10:00:08 -0700

    Added error handling to RateLimits.rb

--- requests/RateLimits.rb
+++ requests/RateLimits.rb
@@ -20,13 +20,23 @@ class RateLimits < TwitterRequest
 
   def success(response)
     log.info("SUCCESS")
+    result    = nil
     all_rates = JSON.parse(response.body)['resources']
-    #puts all_rates.inspect
-    rates     = all_rates[params[:resources]][data[:endpoint]]
-    count     = rates['remaining']
-    time      = Time.at(rates['reset']) + 10
-    data      = { count: count, time: time }
-    yield data
+    # puts all_rates.inspect
+    if all_rates.has_key?(params[:resources])
+      resource_rates = all_rates[params[:resources]]
+      if resource_rates.has_key?(data[:endpoint])
+        rates  = resource_rates[data[:endpoint]]
+        count  = rates['remaining']
+        time   = Time.at(rates['reset']) + 10
+        result = { count: count, time: time }
+      else
+        result = { error: "No rate information for #{data[:endpoint]}" }
+      end
+    else
+      result = { error: "No rate information for #{params[:resources]}" }
+    end
+    yield result
   end
 
 end
